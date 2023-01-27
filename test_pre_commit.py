a3de48989f9239d89b70c842977a39bd7cd4268f
Matthias Hofmann <matthias.j.hofmann@gmx.de>
2023-01-27 08:49:17 +0100

    attempt introduce pre commit

---
+++ test_pre_commit.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 08:46:58 2023
+
+@author: LocalAdmin
+"""
+import os
+import re
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# %%
+
+a =    "sdsd"
+
+b = a + "asas"
